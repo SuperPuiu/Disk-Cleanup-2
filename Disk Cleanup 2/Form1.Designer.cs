91e21df388c63b9c1518d3eb30600a0107f2bd0b
SuperPuiu <97047469+SuperPuiu@users.noreply.github.com>
2022-02-09 20:01:26 +0200

    Disk Cleanup 2 folder
    Disk Cleanup source code

---
+++ Disk Cleanup 2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Disk_Cleanup_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox1.Location = new System.Drawing.Point(12, 165);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(198, 24);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Clear temporary folder";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox2.Location = new System.Drawing.Point(12, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(233, 24);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Clear Software Distribution";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox3.Location = new System.Drawing.Point(12, 109);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(152, 24);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Clear MiniDump";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox4.Location = new System.Drawing.Point(12, 135);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(180, 24);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "Clear InternetCache";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Disk Cleanup 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ Disk Cleanup 2/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Disk_Cleanup_2
+{
+    public partial class Form1 : Form
+    {
+        bool ClearDump = false;
+        bool SoftwareDistribution = false;
+        bool TempFol = false;
+        bool ClearCache = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TempFol)
+            {
+                if (Directory.Exists(Path.GetTempPath()))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(Path.GetTempPath());
+                    foreach (FileInfo file in dir.GetFiles())
+                    {
+                        try
+                        {
+                            file.Delete();
+                        }
+                        catch { }
+                    }
+                    foreach (DirectoryInfo dire in dir.GetDirectories())
+                    {
+                        try
+                        {
+                            dire.Delete(true);
+                        }
+                        catch { }
+                    }
+                }
+            }
+
+            if (SoftwareDistribution)
+            {
+                if (Directory.Exists("C:\\Windows\\SoftwareDistribution"))
+                {
+                    DirectoryInfo drr2 = new DirectoryInfo("C:\\Windows\\SoftwareDistribution");
+                    foreach (FileInfo file in drr2.GetFiles())
+                    {
+                        try
+                        {
+                            file.Delete();
+                        }
+                        catch { }
+
+                        foreach (DirectoryInfo drr in drr2.GetDirectories())
+                        {
+                            try
+                            {
+                                drr.Delete(true);
+                            }
+                            catch { }
+                        }
+                    }
+                }
+            }
+            if (ClearDump)
+            {
+                if (Directory.Exists("C:\\Windows\\minidump"))
+                {
+                    DirectoryInfo drr2 = new DirectoryInfo("C:\\Windows\\minidump");
+                foreach (FileInfo file in drr2.GetFiles())
+                {
+                    try
+                    {
+                        file.Delete();
+                    }
+                    catch { }
+
+                    foreach (DirectoryInfo drr in drr2.GetDirectories())
+                    {
+                        try
+                        {
+                            drr.Delete(true);
+                        }
+                        catch { }
+                    }
+                }
+            }
+        }
+       if(ClearCache)
+            {
+                if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache)))
+                {
+                    DirectoryInfo drr2 = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache));
+                    foreach (FileInfo file in drr2.GetFiles())
+                    {
+                        try
+                        {
+                            file.Delete();
+                        }
+                        catch { }
+
+                        foreach (DirectoryInfo drr in drr2.GetDirectories())
+                        {
+                            try
+                            {
+                                drr.Delete(true);
+                            }
+                            catch { }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            TempFol = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            SoftwareDistribution = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            ClearDump = checkBox3.Checked;  
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            ClearCache = checkBox4.Checked;
+        }
+    }
+}
